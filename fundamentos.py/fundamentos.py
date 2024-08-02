c2e53d9eaec72358c0f06999470647997bf9881a
Miguel Carabali <macarabali1@gmail.com>
2024-08-02 13:15:39 -0500

    se abordaron fundamentos de python

---
+++ fundamentos.py/fundamentos.py
@@ -0,0 +1,10 @@
+name = "Miguel"
+print(type(name))
+print(name[0])
+print(name[1])
+print(name[2])
+last_name = "Gonzalez"
+print(name * 5)
+print(len(name))
+print(name.lower())
+print(name.upper())
