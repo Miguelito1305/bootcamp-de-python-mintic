0eb4ce9edbfc59afb07d9d7464a733a32eebb899
Miguel Carabali <macarabali1@gmail.com>
2024-08-15 08:39:42 -0500

    abordamos operadores de comparacio

---
+++ operadores_de_comparacion.py
@@ -0,0 +1,32 @@
+es_igual_que = 5 == 5
+print(es_igual_que)
+
+es_distinto = 5 != 6
+print(es_distinto)
+
+es_mayor_que = 10 > 10
+print(es_mayor_que)
+
+es_mayor_igual = 10 >= 10
+print(es_mayor_igual)
+
+es_menor_que = 10 < 10
+print(es_menor_que)
+
+es_menor_igual = 10 <= 10
+print(es_menor_igual)
+
+#calculos combinados
+a = 5
+b = 10
+c = 20
+comparacion = a + b == c    
+print(comparacion)
+#comparar usuarios
+contrasena_almacenada = "123456"
+contrasena_escrita="123456"
+print(contrasena_almacenada == contrasena_escrita) #true
+
+contrasena_almacenada = "123456"
+contrasena_escrita='''123456'''
+print(contrasena_almacenada == contrasena_escrita) #true
