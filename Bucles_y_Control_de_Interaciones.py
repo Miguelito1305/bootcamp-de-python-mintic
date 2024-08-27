404ffed78733a4c5d4f264c1f4acad02e7f58b30
Miguel Carabali <macarabali1@gmail.com>
2024-08-27 16:20:12 -0500

    abordamos Bucles

---
+++ Bucles_y_Control_de_Interaciones.py
@@ -0,0 +1,22 @@
+numbers =[1,2,3,4,5,6]
+for i in numbers:
+    print("Aqui i es igual a : ",i+1)
+    for i in range(3, 10):
+        print(i)
+fruits = ["Manzana","Pera","uva","Mango","Naranja"]
+for fruta in fruits:
+    print(fruta)    
+    if fruta == "uva":
+        print("uva encontrada")
+x = 0
+while x < 5:
+    if x == 3:
+        break
+    print(x)
+    x += 1
+    
+numbers = [1,2,3,4,5,6]
+for i in numbers:
+    if i == 3:
+        break
+    print("Aqui i es igual a : ",i+1)
