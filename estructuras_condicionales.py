186c246fc601d5be749e061b9d2e2032dac02528
Miguel Carabali <macarabali1@gmail.com>
2024-08-27 14:53:40 -0500

    abordamos estructuras condicionales

---
+++ estructuras_condicionales.py
@@ -0,0 +1,30 @@
+#x = 5
+#if x > 5:
+ #   print("x es mayor que 5")
+#elif x == 5:
+ #   print("x es igual a 5")
+#else:
+ #   print("x es menor o igual a 5")  
+ 
+"""x = 15
+y = 20
+if x > 10 and y > 25:
+    print("x es mayor que 10 y y es mayor que 25")
+if x > 10 or y > 25:
+    print("x es mayor que 10 o y es mayor que 25")
+if not x > 10:
+    print("x no es mayor que 10")"""
+    
+is_member = True
+age = 1
+
+if is_member:
+    if age >= 15:
+        print("Puedes entrar")
+    else:
+        print("No puedes entrar")
+else:
+    print("No eres mienbro y no tienes acceso")
+    
+   
+   
